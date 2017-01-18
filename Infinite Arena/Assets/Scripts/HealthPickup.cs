293508a5611683f015899fc481e9944c63c34fe1
Albert Vasconcellos <albertvasconcellos@live.ca>
2017-01-18 11:13:21 -0500

    Changelog - Infinite Arena Version 0.0.2
    - Added Player model and 3rd person camera (to be modified)
    - Created HealthPickup.cs and PlayerStats.cs
    - Added models for Stage 1
    - Created Test Scene for testing scripts before implementing them into the Alpha build

---
+++ Infinite Arena/Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour {
+
+    #region Private Variables
+
+    [SerializeField] private float healthToIncrease;
+    [SerializeField] private bool isActive;
+
+    #endregion
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && isActive == true)
+        {
+            other.GetComponent<PlayerStats>().incrementHP(healthToIncrease);
+            //TODO: Play Health Pickup sound here
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Infinite Arena/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    #region Private Variables
+
+    [SerializeField] private float maxHP;
+    [SerializeField] private float _HP;
+
+    #endregion
+
+    #region Getters and Setters
+
+    public void incrementHP(float hpToAdd)
+    {
+        if (hpToAdd + _HP >= maxHP)
+        {
+            _HP = maxHP;
+        }
+        else
+        {
+            _HP = hpToAdd + _HP;
+        }
+    }
+
+    #endregion
+
+    #region Start and Update
+
+    // Use this for initialization
+    void Start () {
+        _HP = maxHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #endregion
+}
